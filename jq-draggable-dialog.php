774e9f532f9a75ea71f00af0be8e839a585bbb0d
ashiryaev <sheinmus@gamil.com>
2010-09-07 00:19:12 +0300

    1

---
+++ jq-draggable-dialog.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.3/themes/base/jquery-ui.css" type="text/css" media="all" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.3/jquery-ui.min.js" type="text/javascript"></script>
+		<script>
+			$(function(){
+				$("#dialog").dialog();
+			})
+		</script>
+	</head>
+	<body>
+		<div id="dialog" title="Basic dialog">
+			<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+		</div>
+	</body>
+</html>
